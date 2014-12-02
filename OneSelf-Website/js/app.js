034663f9ee71f1d4a26a034b54fdc5371aa09c7f
Chinmay Naik <cnaik@equalexperts.com>
2014-12-02 15:09:50 +0530

    - Make the up arrow a link back to #top of page

--- OneSelf-Website/js/app.js
+++ OneSelf-Website/js/app.js
@@ -1,30 +1,35 @@
 function showNavigation(mode) {
-	$(document).ready(function() {
-		if ($(document).width() < 801) {
-			$('#collapse-link').slideToggle(mode);
-		}
-	});
+    $(document).ready(function () {
+        if ($(document).width() < 801) {
+            $('#collapse-link').slideToggle(mode);
+        }
+    });
 }
-$(document).ready(function() {
-	headerResize();
+$(document).ready(function () {
+    headerResize();
+    $('.back-to-top').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 500);
+        return false;
+    })
 });
 
-$(window).resize(function() {
-	headerResize();
+$(window).resize(function () {
+    headerResize();
 });
-$(window).scroll(function() {
-	var headerHeight = $(".header-wrapper").height();
-	console.log($(".page-content").scrollTop());
-	if ($(window).scrollTop() < headerHeight) {
-		$(".page-nav").removeClass("shrink");
-	}
-	else {
-		$(".page-nav").addClass("shrink");
+$(window).scroll(function () {
+    var headerHeight = $(".header-wrapper").height();
+    console.log($(".page-content").scrollTop());
+    if ($(window).scrollTop() < headerHeight) {
+        $(".page-nav").removeClass("shrink");
+    }
+    else {
+        $(".page-nav").addClass("shrink");
 
-	}
+    }
 });
 
 function headerResize() {
-	var headerHeight = $(".header-wrapper").height();
-	$(".page-content").css("padding-top", headerHeight + "px");
+    var headerHeight = $(".header-wrapper").height();
+    $(".page-content").css("padding-top", headerHeight + "px");
 }
