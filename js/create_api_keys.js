8c3ff2356323c57a8eeb8e84b36d42f5aa070873
Douglas Vaz <douglas1992@gmail.com>
2014-12-17 14:38:42 +0530

    Fix multiple requests buug

--- js/create_api_keys.js
+++ js/create_api_keys.js
@@ -1,35 +1,35 @@
 $(document).ready(function(){
+    const API_HOST = "http://localhost:5000";
+
+    var create_api_keys = function(){
+        var appName = $('#appName').val(),
+            appDescription = $('#appDescription').val(),
+            appUrl = $('#appUrl').val(),
+            appEmail = $('#appEmail').val(),
+
+            params = {
+                appName: appName.trim(),
+                appDescription: appDescription.trim(),
+                appUrl: appUrl.trim(),
+                appEmail: appEmail.trim()
+            };
+
+        $.post(API_HOST + "/v1/app", params)
+            .done(function(message){
+                $('#create_key_message').html(message);
+                $('#create_key_message').addClass('alert-success');
+            })
+            .error(function(){
+                $('#create_key_message').html("Something wrong, please try after sometime");
+                $('#create_key_message').addClass('alert-danger');
+            })
+            .always(function() {
+                $('#create_key_message').show();
+            });
+    };
     $("#createAppForm").submit(function(event){
         event.preventDefault();
         create_api_keys();
     });
 });
 
-const API_HOST = "http://localhost:5000";
-
-var create_api_keys = function(){
-    var appName = $('#appName').val(),
-        appDescription = $('#appDescription').val(),
-        appUrl = $('#appUrl').val(),
-        appEmail = $('#appEmail').val(),
-
-        params = {
-            appName: appName.trim(),
-            appDescription: appDescription.trim(),
-            appUrl: appUrl.trim(),
-            appEmail: appEmail.trim()
-        };
-
-    $.post(API_HOST + "/v1/app", params)
-        .done(function(message){
-            $('#create_key_message').html(message);
-            $('#create_key_message').addClass('alert-success');
-        })
-        .error(function(){
-            $('#create_key_message').html("Something wrong, please try after sometime");
-            $('#create_key_message').addClass('alert-danger');
-        })
-        .always(function() {
-            $('#create_key_message').show();
-        });
-};
