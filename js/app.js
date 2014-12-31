eb61a64d1d73efc19006d80b60ee4f72368bc782
Devaroop <devaroop123@yahoo.co.in>
2014-12-31 14:40:48 +0530

    add login popup on website

--- js/app.js
+++ js/app.js
@@ -22,6 +22,21 @@ $(document).ready(function () {
         $('html, body').animate({scrollTop: 0}, 500);
         return false;
     });
+
+    $("#signInForm").submit(function(event){
+        event.preventDefault();
+        checkUserName();
+    });
+
+    $('#loginButton').click(function(event){
+        event.preventDefault();
+        $('#loginBox').show();
+    });
+
+    $('.closeButton').click(function(event){
+        event.preventDefault();
+        $('#loginBox').hide();
+    });
 });
 
 $(window).resize(function () {
@@ -41,3 +56,19 @@ function headerResize() {
     var headerHeight = $(".header-wrapper").height();
     $(".page-content").css("padding-top", headerHeight + "px");
 }
+
+var checkUserName = function(){
+    const LOGIN_ENDPOINT = "http://api.1self.co";
+
+    var username = $('#oneselfUsername').val(),
+
+    url = LOGIN_ENDPOINT + '/v1/user/' + username + "/exists";
+
+    $.get(url)
+        .done(function(){
+            document.location.href = LOGIN_ENDPOINT + '/auth/github';
+        })
+        .error(function(){
+            $('#signinErrorMessage').html("Please enter a valid 1self username");
+        });
+};
