0e1eeaf16017169d67779e12a310974e6fb466d1
Martin <martinstrotton@gmail.com>
2015-08-25 11:01:10 +0100

    back formatting change and draggable overlay on front chart

--- card-stack/js/card-elements.js
+++ card-stack/js/card-elements.js
@@ -156,7 +156,7 @@ function createCardText(cardData, colour) {
 
         cardData.cardText = cardText;
     }
-};
+}
 
 function buildPropertiesTextAndGetValue (propertiesObject) {
     // debugger;
