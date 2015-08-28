e020fa91f2d218753380ecab9da01c1aa4491da6
Martin <martinstrotton@gmail.com>
2015-08-28 09:34:31 +0100

    card loading animation

--- card-stack/js/build-stack.js
+++ card-stack/js/build-stack.js
@@ -8,6 +8,8 @@ function buildStack (stack) {
     deferred.done(function(cardsArray) {
     	globalCardsArray = cardsArray;
 
+        $('.loading-div-top').hide();
+        $('.loading-div-bottom').hide();
         $('.out-of-text').text(cardsArray.length);
         $('.card-number-text').text("1");
         $('.card-count').show();
