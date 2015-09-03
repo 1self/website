d5ac99793783126ad48458c53541c3896f71e2fb
Martin <martinstrotton@gmail.com>
2015-09-03 09:39:38 +0100

    Revert "further debugging"
    This reverts commit ed8413ca811c1b9becef39fe6429aa9b580f6fbd.

--- card-stack/js/card-elements.js
+++ card-stack/js/card-elements.js
@@ -15,7 +15,7 @@ function markCardRead(username, cardElem, cardReloadCount) {
     console.log('enter markCardRead', cardElem);
 
     var cardId = cardElem.getAttribute('cardId');
-    console.log('cardid:', cardId);
+    console.log('cardid:', cardid);
 
     if (cardId) {
         var now = new Date();
